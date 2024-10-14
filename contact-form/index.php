88991762e3c975ca5716b46d65eb44f36f51f91d
mdsafwansabit <157735464+mdsafwansabit@users.noreply.github.com>
2024-10-14 10:11:37 +0600

    Add files via upload

---
+++ contact-form/index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+
+<title> Safwan Sabit - Contact Form </title>
+
+</head>
+
+<body>
+
+<h1> Contact Form </h1>
+
+<form action="form.php" method="post">
+<label> Name: </label>
+<input type="text" name="name" required> <br>
+
+<label> Email: </label>
+<input type="text" name="email" required> <br>
+
+<label> Message: </label>
+<input type="text" name="message" required> <br>
+
+
+<input type="submit" value="Submit">
+
+</form>
+
+
+
+
+</body>
+
+
+</html>
